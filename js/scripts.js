403c471d4c85862dbf39c4018a1b93dffbfc6a5d
atnartur <atnartur@yandex.ru>
2014-06-27 20:03:42 +0400

    Added responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,43 @@
 function dropdown_init() {
     $('#header .nav > li').each(function(){
-        if ($(this).children('.sub').length > 0) {
+        if ($(this).children('.sub').length > 0)
             $(this).addClass('dropdown');
-            //$(this).children('a').after('<div class="arrow"></div>');
-        }
     });
     
     $('#header .nav li.dropdown').hover(function(){
-        $(this).children('.sub').stop().slideDown(100);
+        if ($('#header .nav').hasClass('small') === false)
+            $(this).children('.sub').stop().slideDown(100);
     }, function(){
-        $(this).children('.sub').stop().slideUp(200);
+        if ($('#header .nav').hasClass('small') === false)
+            $(this).children('.sub').stop().slideUp(200);
     });
+}
+
+function menu_toggler_init() {
+    $('#menu_toggler').click(function(){
+        $('#header .nav').slideToggle(200);
+    });
+}
+
+function menu_toggler_resize(){
+    var width = $(window).width();
+    
+    if (width < 800) {
+        if ($('#header .nav').hasClass('small') === false)
+            $('#header .nav').addClass('small');
+    }
+    else{
+        if ($('#header .nav').hasClass('small') === true)
+            $('#header .nav').removeClass('small');
+    }
 }
 
 $(document).ready(function(){
     dropdown_init();
+    menu_toggler_init();
+    menu_toggler_resize();
 })
+
+$(window).resize(function(){
+    menu_toggler_resize();
+});
